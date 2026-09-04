10787679a0cf7b5deff8aca8c140a7ec481c680e
evamode29 <maghighi9201@gmail.com>
2026-09-05 02:06:30 +0330

    Add order URLs

---
+++ orders/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = "orders"
+
+urlpatterns = [
+    path("", views.order_list, name="list"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("success/<int:order_id>/", views.success, name="success"),
+    path("<int:order_id>/", views.order_detail, name="detail"),
+]
