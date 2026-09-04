e5bca22d9f9c915231b94e66c48195ae4f7fb33e
evamode29 <maghighi9201@gmail.com>
2026-09-05 02:15:10 +0330

    Connect cart and orders URLs

--- config/urls.py
+++ config/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("shop/", include("products.urls")),
     path("account/", include("accounts.urls")),
+    path("cart/", include("cart.urls")),
+    path("orders/", include("orders.urls")),
     path("", home, name="home"),
 ]
