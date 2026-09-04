028162631cb58a7cec075b9ab564e3748e483fdd
evamode29 <maghighi9201@gmail.com>
2026-09-05 01:15:09 +0330

    Connect Django homepage to EVA MODE home template

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.http import HttpResponse
 
-
-def home(request):
-    return HttpResponse("<h1 style='font-family:sans-serif;text-align:center;margin-top:20vh'>EVA MODE Django is running ✓</h1>")
+from products.views import home
 
 
 urlpatterns = [
