b81af5112c3c016bb852232f8d1811f8ef313ca5
evamode29 <maghighi9201@gmail.com>
2026-09-05 02:20:39 +0330

    Support color-aware cart item routes

--- cart/urls.py
+++ cart/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("", views.cart_detail, name="detail"),
     path("add/<int:product_id>/", views.add_to_cart, name="add"),
     path("update/<int:product_id>/", views.update_cart, name="update"),
+    path("update/<int:product_id>/<int:color_id>/", views.update_cart, name="update_color"),
     path("remove/<int:product_id>/", views.remove_from_cart, name="remove"),
+    path("remove/<int:product_id>/<int:color_id>/", views.remove_from_cart, name="remove_color"),
 ]
