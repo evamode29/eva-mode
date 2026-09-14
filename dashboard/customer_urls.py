136c484127070f8b4d9e88a2e7cbedfb310b0150
evamode29 <maghighi9201@gmail.com>
2026-09-14 12:59:07 +0330

    Add customer dashboard routes

---
+++ dashboard/customer_urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+
+from .views import CustomerLoginView, customer_dashboard
+
+app_name = "customer"
+
+urlpatterns = [
+    path("login/", CustomerLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("", customer_dashboard, name="dashboard"),
+]
