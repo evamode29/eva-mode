5cb1b6aefb9c24161239b08a098ab1b2830f6a3d
evamode29 <maghighi9201@gmail.com>
2026-09-14 13:00:37 +0330

    Add customer registration route

--- dashboard/customer_urls.py
+++ dashboard/customer_urls.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import CustomerLoginView, customer_dashboard
+from .views import CustomerLoginView, customer_dashboard, register
 
 app_name = "customer"
 
 urlpatterns = [
     path("login/", CustomerLoginView.as_view(), name="login"),
+    path("register/", register, name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("", customer_dashboard, name="dashboard"),
 ]
